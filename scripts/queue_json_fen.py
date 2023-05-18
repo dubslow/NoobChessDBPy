ace985ee8e681f27b9dd092fb97a833726787f02
Dubslow <bunslow@gmail.com>
2023-05-17 22:56:53 -0500

    Common args queue_json_fen

--- scripts/queue_json_fen.py
+++ scripts/queue_json_fen.py
@@ -16,13 +16,17 @@
 #    See the LICENSE file for more details.
 
 '''
-This script reads JSON files, one at a time: read FENs from json of {fen: int}, deduplicate the positions,
+This script reads JSON files, one at a time: read FENs from json of {fen: <blah>}, deduplicate the positions,
 rinse and repeat, and cross-deduplicate between all files. Then mass-queue the cross-deduplicated positions into CDB.
-(Note: queueing near-root positions can be quite expensive, so use this with caution. TODO: write a better form that
-does only some queueing, and some querying instead)
+
+The <blah> is ignored.
+
 Note: queue order is arbitrary.
 
-(One can also queue PGN files, see queue_pgn.py)
+(Note: queueing near-root positions can be quite expensive, so use this with caution. TODO: write a better form that
+does only some queueing, and some querying instead)
+
+One can also queue PGN files, see queue_pgn.py.
 '''
 
 import argparse
@@ -33,8 +37,8 @@ import math
 import trio
 import chess.pgn
 
-from noobchessdbpy.api import AsyncCDBClient
-from noobchessdbpy.library import AsyncCDBLibrary
+from noobchessdbpy.api import strip_fen
+from noobchessdbpy.library import AsyncCDBLibrary, CDBArgs
 
 logging.basicConfig(
     format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
@@ -42,33 +46,39 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-async def parse_and_queue_pgn(args):
-    async with AsyncCDBLibrary(concurrency=args.concurrency) as lib:
-        all_positions, n, u_sub = set(), 0, 0
-        print(f"reading from {args.filenames} ...")
-        for i, filename in enumerate(args.filenames):
-            with open(filename) as filehandle:
-                jfens = json.loads(filehandle.read())
-            x = len(jfens)
-            this_positions = set(jfens.keys())
-            this_len = len(this_positions)
-            print(f"after deduplicating {filename}, found {this_len} unique positions from {x} total, {this_len/x:.2%} unique rate")
-            n += x
-            u_sub += this_len
-            all_positions |= this_positions
-            u = len(all_positions)
-            if i > 0:
-                print(f"after cross-deduplication, found {u} cross-unique positions from {u_sub} sub-unique from {n} "
-                      f"total, {u/n if n else math.nan:.2%} unique rate")
+
+def parse_json_fens(args):
+    all_positions, n, u_sub = set(), 0, 0
+    print(f"reading from {args.filenames} ...")
+    for i, filename in enumerate(args.filenames):
+        with open(filename) as filehandle:
+            jfens = json.loads(filehandle.read())
+        x = len(jfens)
+        this_positions = set(strip_fen(fen) for fen in jfens.keys())
+        this_len = len(this_positions)
+        print(f"after deduplicating {filename}, found {this_len} unique positions from {x} total, {this_len/x:.2%} unique rate")
+        n += x
+        u_sub += this_len
+        all_positions |= this_positions
+        u = len(all_positions)
+        if i > 0:
+            print(f"after cross-deduplication, found {u} cross-unique positions from {u_sub} sub-unique from {n} total,"
+                  f" {u/n if n else math.nan:.2%} unique rate")
+    return all_positions
+
+async def mass_queue_set(args, all_positions):
+    async with AsyncCDBLibrary(concurrency=args.concurrency, user=args.user) as lib:
         await lib.mass_queue_set(all_positions)
 
+def main(args):
+    all_positions = parse_json_fens(args)
+    trio.run(mass_queue_set, args, all_positions)
+
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('filenames', nargs='+', help='A list of filenames to read JSON FEN from')
+CDBArgs.add_api_args_to_parser(parser)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('filenames', nargs='+',
-                        help="""A list of filenames to read JSON FEN from. """)
-    parser.add_argument('-c', '--concurrency', type=int, default=AsyncCDBClient.DefaultConcurrency,
-                                                      help="maximum number of parallel requests (default: %(default)s)")
-
     args = parser.parse_args()
-    trio.run(parse_and_queue_pgn, args)
+    main(args)
