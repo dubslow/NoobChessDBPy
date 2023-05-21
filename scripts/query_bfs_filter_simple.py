2a898e1fe363bc35a58a2cd96102e96017c6301e
Dubslow <bunslow@gmail.com>
2023-05-21 07:14:00 -0500

    minor bfs_filter fix

--- scripts/query_bfs_filter_simple.py
+++ scripts/query_bfs_filter_simple.py
@@ -45,6 +45,7 @@ import math
 import trio
 import chess
 
+from noobchessdbpy.api import CDBStatus
 from noobchessdbpy.library import AsyncCDBLibrary, BreadthFirstState, CDBArgs
 
 logging.basicConfig(
@@ -59,6 +60,8 @@ def well_biased_filter(board:chess.Board, cdb_json):
     a position whose top two moves are in the range [90, 112]'''
     # Refer to AsyncCDBClient.query_all docstring, or the CDB website, for a quick format reference.
     # Note that we don't actually use the `board` arg here, but ofc some may find use for it
+    if cdb_json['status'] is not CDBStatus.Success:
+        return False
     moves = cdb_json['moves']
     if len(moves) < 5: return False # Guarantee some minimum quality of analysis
     t1, t2 = moves[0:2]
